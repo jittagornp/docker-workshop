034b22499e4569e277efc175cfae5b460bd003c0
jittagornp <jittagornp@gmail.com>
2020-04-21 22:53:15 +0700

    add build-and-run-vuejs

---
+++ build-and-run-vuejs/frontend/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -rf dist
+
+#build source code (.jar file)
+docker run --rm -v $(pwd):/web node:12.13.0 cd frontend && yarn install && yarn build
+
+# remove container and images
+docker stop vuejs-frontend || true
+docker rm vuejs-frontend || true
+docker rmi vuejs-frontend || true
+
+# build docker image
+docker build -t vuejs-frontend .
+
+# run container
+docker run -d --name=vuejs-frontend -p 3001:80 --restart=always vuejs-frontend
