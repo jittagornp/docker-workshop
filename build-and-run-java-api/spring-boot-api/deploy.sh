d260ef3e36599e0d5c755f21d775102a969fac8b
jittagornp <jittagornp@gmail.com>
2020-04-21 21:14:56 +0700

    add build-and-run-java-api

---
+++ build-and-run-java-api/spring-boot-api/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -rf target
+
+#build source code (.jar file)
+docker run --rm -v $(pwd):/app -v /root/.m2:/root/.m2 maven:3.6.2-jdk-11 mvn install -f /app/pom.xml
+
+# remove container and images
+docker stop spring-boot-api || true
+docker rm spring-boot-api || true
+docker rmi spring-boot-api || true
+
+# build docker image
+docker build -t spring-boot-api .
+
+# run container
+docker run -d --name=spring-boot-api -p 3002:80 --restart=always spring-boot-api
