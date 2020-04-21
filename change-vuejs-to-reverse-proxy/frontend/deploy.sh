528898d84933a4831d64d82b8f46011101af228a
jittagornp <jittagornp@gmail.com>
2020-04-21 23:46:38 +0700

    add build-and-run-vuejs

---
+++ change-vuejs-to-reverse-proxy/frontend/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf dist
+
+#build source code (.jar file)
+docker run --rm -v $(pwd):/web node:12.13.0 yarn --cwd ./web/ install 
+docker run --rm -v $(pwd):/web node:12.13.0 yarn --cwd ./web/ build 
+
+# remove container and images
+docker stop vuejs-frontend || true
+docker rm vuejs-frontend || true
+docker rmi vuejs-frontend || true
+
+# build docker image
+docker build -t vuejs-frontend .
+
+# run container
+docker run -d --name=vuejs-frontend -p 80:80 --restart=always --link=spring-boot-api vuejs-frontend
